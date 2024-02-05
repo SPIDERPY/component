09259346f4e05fa7f959cbb4a9907d2f3513c079
AB <ab@AB.lan>
2024-02-05 11:05:11 +0100

    Des boîtes aux quatre coins

---
+++ src/app/block/block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.css'
+})
+export class BlockComponent {
+  title = "CodeSandbox"
+
+}
